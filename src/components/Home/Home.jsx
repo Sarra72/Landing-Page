6884a8823cc471bb30a4369337c6bf24a2856521
Sara AbdelKader <sarahmedd72@gmail.com>
2025-04-26 00:05:49 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
       <div className="home-bd container">
-        <img src="././src/assets/spices.png" alt="spices image" className='spices' />
-        <img src="././src/assets/onion-and-tomato.png" alt="onion-and-tomato image" className='onion-and-tomato' />
+        <img src="spices.png" alt="spices image" className='spices' />
+        <img src="onion-and-tomato.png" alt="onion-and-tomato image" className='onion-and-tomato' />
         <div className="mid">
           <div className="mid-f">
             <p className='headr'>Take a taste <br /> Come join us.</p>
@@ -13,7 +13,7 @@ export default function Home() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique illum aliquam quam velit enim atque optio iusto rerum perferendis neque Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates ea velit error deserunt molestiae totam sunt temporibus sit ut vel.</p>
             <button className='explore-now'>Explore Now</button>
           </div>
-            <img src="././src/assets/main-img.png" alt="sushi image" className='suhsi' />
+            <img src="main-img.png" alt="sushi image" className='suhsi' />
            </div>
       </div>
     </>
